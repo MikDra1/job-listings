548481a434540fd3fa82942fa61e0a6c4f93ddff
MikDra1 <mikolajkuleczka@gmail.com>
2024-08-31 13:49:42 +0200

    Initial commit

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -7,7 +7,7 @@ function Filter({filter, deleteFilter}) {
     return (
         <div className="filter">
             {filter}
-            <span onClick={() => deleteFilter(filter)} className="filter__delete"><img src="../../public/images/icon-remove.svg" alt="Image to close the filter" /></span>
+            <span onClick={() => deleteFilter(filter)} className="filter__delete"><img src="../images/icon-remove.svg" alt="Image to close the filter" /></span>
         </div>
     )
 }
